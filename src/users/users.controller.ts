8b3f9957f6cf13ce2cfbd357484c454b257f3788
javierdebug <jvargas071@gmail.com>
2026-03-07 03:21:12 -0500

    Update and add Users controller, service and dto

---
+++ src/users/users.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Put,
+  Param,
+  Delete,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Get()
+  findAll() {
+    return this.usersService.findAllUsers();
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const existingUser = await this.usersService.findUserById(id);
+    if (!existingUser) {
+      throw new NotFoundException();
+    }
+    return existingUser;
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    const newUser = await this.usersService.createUser(createUserDto);
+    return newUser;
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    const updatedUser = await this.usersService.updateUser(id, updateUserDto);
+    return updatedUser;
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.usersService.deleteUser(id);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,21 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { SignUpDto } from 'src/auth/dto/signUp.dto';
 
 export interface User {
-  userId: number;
+  userId: string;
   username: string;
   email: string;
   password: string;
 }
 
+//TODO: mock data, remove later
 const usersMock: User[] = [
   {
-    userId: 1,
+    userId: randomUUID(),
     username: 'Javier',
     email: 'javier@javierd.com',
     password: 'password',
   },
   {
-    userId: 2,
+    userId: randomUUID(),
     username: 'Milena',
     email: 'milena@javierd.com',
     password: 'passwordMilena',
@@ -24,7 +31,66 @@ const usersMock: User[] = [
 
 @Injectable()
 export class UsersService {
+  async findAllUsers(): Promise<User[] | undefined> {
+    return usersMock;
+  }
+
+  async findUserById(id: string): Promise<User | undefined> {
+    const existingUser = await usersMock.find((user) => user.userId === id);
+    return existingUser;
+  }
+
   async findUserByName(username: string): Promise<User | undefined> {
-    return await usersMock.find((user) => user.username === username);
+    const existingUser = await usersMock.find(
+      (user) => user.username === username,
+    );
+    return existingUser;
+  }
+
+  async findUserByEmail(email: string): Promise<User | undefined> {
+    const existingUser = await usersMock.find((user) => user.email === email);
+    return existingUser;
+  }
+
+  async createUser(user: SignUpDto): Promise<User> {
+    const existingUser = await this.findUserByEmail(user.email);
+    if (existingUser) {
+      throw new BadRequestException('Email already registered');
+    }
+
+    const newUser: User = {
+      userId: randomUUID(),
+      ...user,
+    };
+    usersMock.push(newUser);
+    return newUser;
+  }
+
+  async updateUser(id: string, user: SignUpDto): Promise<User> {
+    const existingUser = await this.findUserById(id);
+    if (!existingUser) {
+      throw new NotFoundException('User not registered');
+    }
+
+    const updatedUser: User = {
+      userId: existingUser.userId,
+      email: user.email,
+      username: user.username,
+      password: user.password,
+    };
+    await this.deleteUser(id);
+    usersMock.push(updatedUser);
+    return updatedUser;
+  }
+
+  async deleteUser(id: string): Promise<void> {
+    const existingUser = await this.findUserById(id);
+    if (!existingUser) {
+      throw new NotFoundException('User not registered');
+    }
+
+    const newUsersMock = usersMock.filter((user) => user.userId !== id);
+    usersMock.length = 0;
+    usersMock.push(...newUsersMock);
   }
 }
