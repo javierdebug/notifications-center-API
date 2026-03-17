ddb67c14d050fd919b11e71107a16931166e6c29
javierdebug <jvargas071@gmail.com>
2026-03-17 13:52:18 -0500

    Update app module with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,24 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './users/entity/user.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT!,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASS,
+      database: process.env.DATABASE_NAME,
+      entities: [UserEntity],
+      synchronize: Boolean(process.env.DATABASE_SYNC),
     }),
     UsersModule,
     AuthModule,
